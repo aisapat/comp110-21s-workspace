f2fbd7ba423aee4fc610dd5c4eab4adee4584ec8
Isabella Patterson <aisapat@email.unc.edu>
2021-02-04 13:29:13 -0500

    progress on puzzle

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730387431"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,19 @@ from random import randint
 
 
 # Begin your solution here...
+print("Your fortune cookie says...")
+
+cookie_number: int = randint(1,4)
+
+if cookie_number < 2:
+    print("Fortune Cookie Message 1")
+else:
+    if cookie_number < 3:
+        print("Fortune Cookie Message 2")
+    else:
+        if cookie_number < 4:
+            print("Fortune Cookie Message 3")
+        else:
+            print("Fortune Cookie Message 4")
+
+print("Now, go spread positive vibes!")
--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -1,6 +1,19 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730387431"
 
 
 # Begin your solution here...
+guess: int = int(input("Enter an int: "))
+
+if guess % 2 == 0 and guess % 7 != 0:
+    print("TAR")
+else:
+    if guess % 7 == 0 and guess % 2 != 0:
+        print("HEELS")
+    else:
+        if guess % 2 == 0 and guess % 7 == 0:
+            print("TAR HEELS")
+        else:
+            print("CAROLINA")
+
